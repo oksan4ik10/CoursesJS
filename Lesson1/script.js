9abd829ffdf374f454028b448aba93083d4f5739
Kalinina Oksana <ilovehamster@mail.ru>
2020-03-29 20:19:59 +0300

    lesson2

--- Lesson1/script.js
+++ Lesson1/script.js
@@ -1,3 +1,14 @@
-let money, income, addExpenses, deposit, mission, period;
-alert("I'm programmer");
-console.log("Hello, people");
+let money=57000, 
+income="freelance", 
+addExpenses="Courses, internet, car", 
+deposit=true,
+ mission=7000000, 
+ period=12,
+ budgetDay=money/30;
+
+console.log(typeof(money));
+console.log(typeof(income));
+console.log(typeof(deposit));
+console.log(addExpenses.length);
+console.log(addExpenses.toLowerCase().split(", "));
+console.log(budgetDay);

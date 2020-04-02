e50c7a47b2ff12b20cc0c5b3afc6c921a88bd3cd
Kalinina Oksana <ilovehamster@mail.ru>
2020-04-02 21:13:01 +0300

    v 1.0

---
+++ Lesson6/script.js
@@ -0,0 +1,23 @@
+// function isNum(n){
+//     return !isNaN(parseFloat(n)) && isFinite(n);
+// }
+// console.log(isNum(prompt()))
+function game(){
+    let num=150;
+    let user=prompt("Угадай число от 1 до 100");
+    
+    if ((user===null)||(user==num)){
+        return;
+    } else{
+        if (num>user) {
+            alert("Загаданное число больше")
+        } else if(num<user){
+            alert("Загаданное число меньше")
+        } else if (isNaN(user)){
+            alert("Вы ввели не число!")
+        } 
+        game();
+    }
+}
+
+game();

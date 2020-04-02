3f0d63a5189d7251620e528c866eec029b12f2e4
Kalinina Oksana <ilovehamster@mail.ru>
2020-04-02 22:40:03 +0300

    She is working! Lesson6

--- Lesson6/script.js
+++ Lesson6/script.js
@@ -1,28 +1,39 @@
-// function isNum(n){
-//     return !isNaN(parseFloat(n)) && isFinite(n);
-// }
-// console.log(isNum(prompt()))
+function isNum(n){
+    return !isNaN(parseFloat(n)) && isFinite(n);
+}
+
+
 function game(){
-    let num=150;
-    let user=prompt("Угадай число от 1 до 100");
-    
-    if (user===null){
-        return;
-    } 
-    else if (user==num){
-        alert("Вы угадали! Ура");
-        return;
-    }
-    else{
-        if (num>user) {
-            alert("Загаданное число больше")
-        } else if(num<user){
-            alert("Загаданное число меньше")
-        } else if (isNaN(user)){
-            alert("Вы ввели не число!")
+    let num=Math.round(Math.random()*100);
+    console.log(num);
+    function change(){
+        let user=prompt("Угадай число от 1 до 100");
+        if (isNum(user)){   
+            if (+user===num){
+                alert("Ты выиграл! ура :)")
+                return;
+            } else{
+                if (num>user) {
+                    alert("Загаданное число больше")
+                } else if(num<user){
+                    alert("Загаданное число меньше")
+                } 
+                change();
+            }
+        }
+        else{
+            if (user===null){
+                alert ("Игра закочена..");
+                return;
+            } 
+            else {
+                alert("Вы ввели не число!")
+            }
+            change();
         } 
-        game();
     }
+    return change();
+    
 }
 
 game();
